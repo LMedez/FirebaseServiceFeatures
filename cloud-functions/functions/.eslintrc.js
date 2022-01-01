561175051927dd690881eea021f0a335f03cb690
Lucho <luchi_m.022@hotmail.com>
2022-01-01 14:35:34 -0300

    new cloud function

--- cloud-functions/functions/.eslintrc.js
+++ cloud-functions/functions/.eslintrc.js
@@ -27,5 +27,7 @@ module.exports = {
   rules: {
     "quotes": ["error", "double"],
     "import/no-unresolved": 0,
+    // windows linebreaks when not in production environment
+    "linebreak-style": ["error", process.env.NODE_ENV === 'prod' ? "unix" : "windows"]
   },
 };
--- cloud-functions/functions/src/index.ts
+++ cloud-functions/functions/src/index.ts
@@ -1,10 +1,28 @@
 import * as functions from "firebase-functions";
+import * as admin from "firebase-admin";
+
+export const sendMessageToTopic = functions.firestore.document("users")
+    .onWrite((change, context) => {
+        // If we set `/users/marie/incoming_messages/134` to {body: "Hello"} then
+        // context.params.userId == "marie";
+        // context.params.messageCollectionId == "incoming_messages";
+        // context.params.messageId == "134";
+        // ... and ...
+        // change.after.data() == {body: "Hello"}
+
+        const payload = {
+            notification: {
+                title: "new news!",
+                body: "new news for you."
+            }
+        }
+        admin.messaging().sendToTopic("news", payload).then(value => {
+            functions.logger.log(value)
+        }).catch(error => {
+            functions.logger.log(error)
+        })
+    });
 
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+
+
