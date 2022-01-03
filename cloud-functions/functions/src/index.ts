1bf505ba94a2e8afb062555777b492a4ad055e3d
Lucho <luchi_m.022@hotmail.com>
2022-01-02 22:12:34 -0300

    create a message function

--- cloud-functions/functions/src/index.ts
+++ cloud-functions/functions/src/index.ts
@@ -1,8 +1,16 @@
 import * as functions from "firebase-functions";
 import * as admin from "firebase-admin";
 
-export const sendMessageToTopic = functions.firestore.document("users")
-    .onWrite((change, context) => {
+admin.initializeApp({
+    credential: admin.credential.applicationDefault()
+})
+
+// the id will be modified be getting the user Authentication id,
+// for praticing reasons i use the id of the unique document in firestore
+
+
+export const sendMessageOnSubscribeToTopic = functions.firestore.document("users/dHqwQqswQ9nGh3nocz80")
+    .onUpdate((change, context) => {
         // If we set `/users/marie/incoming_messages/134` to {body: "Hello"} then
         // context.params.userId == "marie";
         // context.params.messageCollectionId == "incoming_messages";
@@ -10,19 +18,19 @@ export const sendMessageToTopic = functions.firestore.document("users")
         // ... and ...
         // change.after.data() == {body: "Hello"}
 
+        const document = change.after.data()
+        const token = document.notificationToken.token
+
         const payload = {
             notification: {
-                title: "new news!",
-                body: "new news for you."
+                title: "Thanks for subscribing!",
+                body: `You are now subscibed to topic ${document.topics[0]}`
             }
         }
-        admin.messaging().sendToTopic("news", payload).then(value => {
+        admin.messaging().sendToDevice(token, payload).then(value => {
             functions.logger.log(value)
         }).catch(error => {
             functions.logger.log(error)
         })
     });
-
-
-
 
